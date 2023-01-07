52cce49f09260925c5cd8e6a894024b8dde4bb5d
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2023-01-07 16:55:23 -0500

    Config urls

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/',include('apps.users.api.routers')),
+    path('users/',include('users.api.routers')),
 ]
